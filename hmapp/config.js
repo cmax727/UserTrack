1a8fb4cd25128c65c0a21ebebcc052c46897db0e
sasya8080 <sasya8080@gmail.com>
2013-09-23 01:12:35 +0800

    ----- config.js : server side configure

--- hmapp/config.js
+++ hmapp/config.js
@@ -6,26 +6,31 @@ module.exports = function(env){
   config.screenshotsPath=__dirname+ "/public/screenshots/";
   config.shortScreenshotsPath="/screenshots/";
   config.port=8080;
-  //config.wkhtmltoimgPath=__dirname+"/utilities/wkhtmltoimage-amd64";
-  config.wkhtmltoimgPath=__dirname+"/utilities/win/wkhtmltoimage.exe";
 
-  //config.phantomPath=__dirname+'/utilities/phantomjs64/bin/phantomjs';
-  config.phantomPath=__dirname+'/utilities/win/phantomjs64/phantomjs.exe';
-
+  config.os = "win";
+  
+  if ( config.os == "win"){
+	  config.wkhtmltoimgPath=__dirname+"/utilities/wkhtmltoimage-amd64";
+	  config.phantomPath=__dirname+'/utilities/phantomjs64/bin/phantomjs';
+  }else{
+	config.wkhtmltoimgPath=__dirname+"/utilities/win/wkhtmltoimage.exe";
+	config.phantomPath=__dirname+'/utilities/win/phantomjs64/phantomjs.exe';
+  }
   config.phantomCookiePath=__dirname+'/utilities/phantomjs64/bin/cookies.txt';
-    
+	
   if(env=='development'){
     //config.wkhtmltoimgPath=__dirname+"/utilities/wkhtmltoimage";
     config.connectionString='mongodb://mounir:mounir@ds033457.mongolab.com:33457/moomap';
-    config.socketConnectionString='http://localhost:8080/';  //Connections string in the userscript
-    //config.phantomPath=__dirname+'/utilities/phantomjs86/bin/phantomjs';
+    config.socketConnectionString='http://hmapp-mstrig.dotcloud.com/';  //Connections string in the userscript
+//    config.socketConnectionString='http://localhost:8080/';  //Connections string in the userscript
+
+	//config.phantomPath=__dirname+'/utilities/phantomjs86/bin/phantomjs';
     //config.phantomCookiePath=__dirname+'/utilities/phantomjs86/bin/cookies.txt';
   }
   
   else if(env=='production'){
     config.connectionString='mongodb://mounir:mounir@ds033457.mongolab.com:33457/moomap';
     config.socketConnectionString='http://hmapp-mstrig.dotcloud.com/'; //Connections string in the userscript
-    
   }
   else{
     throw new Error('no env set');
